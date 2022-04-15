f7d7f218274267be77d7a71fc3c7c827fad3ca5c
segundo rebaza <alvaro.rp1593@gmail.com>
2022-04-15 18:43:09 -0500

    finish geometrics figures

--- Practice/figures.js
+++ Practice/figures.js
@@ -11,3 +11,30 @@ const perimetroCirculo = (radio) => {
   return diametro * PI;
 }
 const areaCirculo = (radio) => (radio * radio) * PI;
+
+// Here we interact with HTML
+const calcularPerimetroCuadrado = () => {
+  const input = document.getElementById("InputCuadrado");
+  const value = input.value;
+
+  const perimetro = perimetroCuadrado(value);
+  alert(perimetro)
+}
+
+const calcularAreaCuadrado = () => {
+  const input = document.getElementById("InputCuadrado");
+  const value = input.value;
+
+  const area = areaCuadrado(value);
+  alert(area)
+}
+
+const alturaIsosceles = (lado1, lado2, base) => {
+  if (lado1 === lado2 && lado1 !== base ) {
+    const altura = Math.sqrt(lado1 ** 2 - (base ** 2)/4)
+    alert(`This is a isosceles triangle, its height is ${altura}`)
+  }
+  else {
+    alert("This is not a isosceles triangle")
+  }
+}
