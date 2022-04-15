88fc4c389695e81e3e05cd53c3a9f5387d3cd0ea
segundo rebaza <alvaro.rp1593@gmail.com>
2022-04-15 18:06:16 -0500

    add functions geometrics

--- Practice/figures.js
+++ Practice/figures.js
@@ -1,1 +1,13 @@
-console.log("Hello")
+const PI = Math.PI
+const perimetroCuadrado = (lado) => lado * 4
+const areaCuadrado = (lado) => lado * lado
+
+const perimetroTriangulo = (lado1, lado2, base) => lado1 + lado2 + base
+const areaTriangulo = (base, altura) => (base * altura) / 2
+
+const diametroCirculo = (radio) => radio * 2
+const perimetroCirculo = (radio) => {
+  const diametro = diametroCirculo(radio)
+  return diametro * PI;
+}
+const areaCirculo = (radio) => (radio * radio) * PI;
